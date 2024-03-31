2776c579326ac5f6f3978030cfdb00e829c47787
MattCzar13 <mattczar13@gmail.com>
2024-03-30 22:34:38 -0400

    main.py created

---
+++ main.py
@@ -0,0 +1,19 @@
+# Matthew Czarnowski
+# 101151676
+#
+# COMP3005 Project V2
+
+import os
+os.system('cls')
+
+# main
+def main():
+	print("Initializing program...")
+	init()
+
+# first time setup
+def init():
+	print("...done.")
+
+if __name__ == "__main__":
+	main()
