273543feffe4ac0108f64b2aa9eb9a9092609207
MattCzar13 <mattczar13@gmail.com>
2024-04-09 19:24:49 -0400

    Basic database connection / login
    Before the "fitness club" part starts, there's an initial prompt to login to your local database for the program to use. Then, it uses the fitness club database to make a login screen, which uses the credentials table to log a user into the program.

--- main.py
+++ main.py
@@ -6,27 +6,86 @@
 import os
 os.system('cls')
 
+import psycopg
+
+connection = None
+cursor = None
+
+username = ""
+password = ""
+logged_in = False
+
 # main
 def main():
 	print("Initializing program...")
-	init()
+	
+	# connect to postgres
+	while not init():
+		pass
+	
+	# repeat user login prompt until the user has logged in with a valid user/pass combo
+	while not login():
+		pass
+		
+	os.system('cls')
+	print("Logged in as " + username + ".")
+
+	# database disconnect, program ends here
+	connection.close()
+	print("Database disconnected / not found. Shutting down.")
+	exit()
 
 # first time setup
-def init():
-	connect_to_database()
+def init() -> bool:
+	global connection
+	global cursor
 
-def connect_to_database() -> bool:
-	# try to connect to the database using psycopg
-	# if something goes wrong, print an error and terminate the program
+	os.system('cls')
+	
+	print("Enter your database information, so this program can connect with it.")
+	print("\n")
+	print("This depends on how you set your database up in pgAdmin.")
+	print("The way it was instructed to be set up in class was:")
+	print("dbname = test_db")
+	print("user = postgres")
+	print("pass = postgres")
+	print("\n")
 
-	# connection code here
-	connection_successful : bool = True
+	connection_dbname = input("DBNAME: ")
+	connection_dbuser = input("DBUSER: ")
+	connection_dbpass = input("DBPASS: ")
+
+	try:
+		connection = psycopg.connect(f"dbname={connection_dbname} user={connection_dbuser} password={connection_dbpass}")
+		cursor = connection.cursor()
+	except:
+		return False
 	
-	connection_successful = False
+	return True
+
+def login() -> bool:
+	global username
+	global password
+
+	os.system('cls')
 	
-	if not connection_successful:
-		print("[!] Something went wrong connecting to the database. :(")
-		exit()
+	print("For ease of access, here is a list of all users' login information:")
+	cursor.execute("SELECT * FROM credentials")
+	for item in cursor:
+		print(item)
+	print("\n")
+	
+	username = input("USER: ")
+	password = input("PASS: ")
+	
+	# see if that username + password combo exists
+	SQL = "SELECT * FROM credentials WHERE credentials.user_name = (%s) AND credentials.user_password = (%s)"
+	data = (username, password)
+	cursor.execute(SQL, data)
+	for item in cursor:
+		return True
+	
+	return False
 
 if __name__ == "__main__":
 	main()
